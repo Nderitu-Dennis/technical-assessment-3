ed2c9fdb2d030f33b0ff46b3d910c689ef3d8b43
Nderitu-Dennis <nderitudenis87@gmail.com>
2025-12-01 17:22:51 +0300

    refactor LeaveApplicationController

--- src/main/java/tech/csm/controller/LeaveApplicationController.java
+++ src/main/java/tech/csm/controller/LeaveApplicationController.java
@@ -66,9 +66,10 @@ private LeaveApplicationService leaveApplicationService;
 // save leave application
 @PostMapping("/save-application")
 public String saveLeaveApplication(@ModelAttribute LeaveApplication application, RedirectAttributes rd) {
-	 // 1. Calculate total days from fromDate and toDate
+	 // Calculate total days from fromDate and toDate
     long days = ChronoUnit.DAYS.between(application.getFromDate(), application.getToDate()) + 1;
     application.setTotalDays((double) days);
+    
 	LeaveApplication  saved = leaveApplicationService.saveLeaveApplication(application);
   String msg = "leave application  saved with id : " + saved.getLeaveApplicationId();
    rd.addFlashAttribute("msg", msg);
@@ -81,58 +82,5 @@ public String saveLeaveApplication(@ModelAttribute LeaveApplication application,
        
    
  
-
-//    //  Get Projects by Team only active & bill_able
-//    @GetMapping("/projects-by-team-id")
-//    @ResponseBody
-//    public List<Project> getProjectsByTeam(@RequestParam("teamId") Integer teamId) {
-//        return projectService.getBillableProjectsByTeam(teamId);
-//    }
-
-
-    
-//    //get all assigned projects
-//    @GetMapping("/get-assigned-projects-list")
-//    public String getProjectsAssigned(Model model) {
-//        List<ProjectAssignment> assignments = projectAssignmentService.getAllAssignedProjects();
-//        if (assignments == null) {
-//            assignments = Collections.emptyList();
-//        }
-//        model.addAttribute("projectsAssigned", assignments);  //variable names shld not contain dashes
-//        return "assigned-projects-list";
-//    }
-
-
-//    // 5. Delete Project Assignment
-//    @GetMapping("/delete-assignment")
-//    public String deleteProjectAssignment(@RequestParam("assignmentId") Integer assignmentId, RedirectAttributes rd) {
-//        projectAssignmentService.deleteAssignment(assignmentId);
-//        rd.addFlashAttribute("msg", "Project deleted successfully!");
-//        return "redirect:./get-assigned-projects-list";
-//    }
-    
-//    //updating/edit a project assignment
-//    @GetMapping("/update-assignment")
-//    public String editAssignment(@RequestParam("assignmentId") Integer assignmentId, Model model) {
-//        ProjectAssignment assignment = projectAssignmentService.getAssignmentById(assignmentId);
-//        
-//        // extract selected IDs  
-//        Integer selectedTeamId = assignment.getProject().getTeam().getTeamId();
-//        Integer selectedDepartmentId = assignment.getProject().getTeam().getDepartment().getDepartmentId();
-//        Integer selectedProjectId = assignment.getProject().getProjectId();
-//        Integer selectedEmployeeId = assignment.getEmployee().getEmployeeId();
-//
-//        model.addAttribute("assignment", assignment); // pre-fill the form
-//        model.addAttribute("selectedDepartmentId", selectedDepartmentId);
-//        model.addAttribute("selectedTeamId", selectedTeamId);
-//        model.addAttribute("selectedProjectId", selectedProjectId);
-//        model.addAttribute("selectedEmployeeId", selectedEmployeeId);
-//        
-//        //  add necessary dropdown list data
-//        model.addAttribute("departments", departmentService.getActiveDepartments());
-//        model.addAttribute("employees", employeeService.getActiveEmployees());
-//        
-//        return "project-assignment-form"; 
-//    }
 
 
